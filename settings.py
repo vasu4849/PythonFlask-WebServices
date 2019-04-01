9861706416a2c45ca30e6f7d529add2c8e34ae53
vnakka <vnakka@factset.com>
2019-04-01 14:29:32 +0530

    updated requirements.txt and settings py files and added database file

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 #specify the path to the DB 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Automation\\My work\\python\\PythonFlask\\database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\test\\python\\PythonFlask-WebServices\\database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
